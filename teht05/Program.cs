14c83b5f1d8d112cc345132edbe0bee754e33ec0
Otto Savolainen <k2534@student.jamk.fi>
2017-01-19 14:09:12 +0200

    tehtävä 5 tehty

---
+++ teht05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sekunnit, minuutit, tunnit;
+
+            Console.Write("Anna sekunnit > ");
+            string line = Console.ReadLine();
+            sekunnit = int.Parse(line);
+
+            tunnit = sekunnit / 3600;
+            minuutit = tunnit % 60;
+            sekunnit = minuutit % 60;
+
+            Console.WriteLine("Antamasi sekunttiaika voidaan ilmaista muodossa: {0} tunti(a) {1} minuutti(a) {2} sekunti(a) ", tunnit, minuutit, sekunnit);
+
+        }
+    }
+}
