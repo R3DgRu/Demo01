dc86d2523214066d81e71b0fa26baafb1baa9d9c
Otto Savolainen <k2534@student.jamk.fi>
2017-01-25 19:54:23 +0200

    tehtäviä lisää

---
+++ teht09/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku; // muuttujia, jipii!
+            int summa = 39; // summamuuttuja
+
+            Console.Write("Anna luku > ");  // kysytään käyttäjältä luku
+            string line = Console.ReadLine();
+            luku = int.Parse(line);
+
+            while (luku != 0)
+            {
+                Console.Write("Anna luku > ");  // kysytään käyttäjältä luku
+                string line1 = Console.ReadLine();
+                luku = int.Parse(line1);
+            }
+
+            Console.WriteLine("Lukujen summa on: {0}", summa); // tulostus, yhteenlaskutoiminto puuttuu vielä
+
+        }
+    }
+}
